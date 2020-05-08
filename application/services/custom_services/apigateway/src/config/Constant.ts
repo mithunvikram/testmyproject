5b9bc2281760f9afe3e4fb95973937d37e8027ab
mithunvikram <undefined>
2020-05-08 07:10:30 +0000

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://testmyproject-app.testmyproject.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://testmyproject-app.testmyproject.svc.cluster.local:3009';
+export const ADMINURL = 'http://testmyproject-app.testmyproject.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://testmyproject-app.testmyproject.svc.cluster.local:3008';
